cc9d34d68e14874126efe8e3b50452f010074e51
alexis guerra <65685428+alexis-guerra@users.noreply.github.com>
2023-11-10 22:46:19 -0800

    Inicio

--- TEMPLATE/Cabecera.php
+++ TEMPLATE/Cabecera.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ALIGNED</title>
+    <title>IMPERIAL</title>
     <link rel="stylesheet" href="./CSS2.0/bootstrap.min.css" />
     <link rel="stylesheet" href="./CSS2.0/estilo.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Aligned</a>
+    <a class="navbar-brand" href="#">IMPERIAL</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
