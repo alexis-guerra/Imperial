6f06e3c04bb9ba6c2d5ae04a89309bb073b4ddd9
alexis guerra <65685428+alexis-guerra@users.noreply.github.com>
2023-11-15 10:57:15 -0800

    barra

--- inicio.php
+++ inicio.php
@@ -1,4 +1,3 @@
-<?php include("template/cabecera.php");?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,12 +15,43 @@
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
 
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="assets/css/fontawesome.css">
-    <link rel="stylesheet" href="assets/css/templatemo-edu-meeting.css">
-    <link rel="stylesheet" href="assets/css/owl.css">
-    <link rel="stylesheet" href="assets/css/lightbox.css">
 <!--
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+<!-- Additional CSS Files -->
+<link rel="stylesheet" href="assets/css/fontawesome.css">
+<link rel="stylesheet" href="assets/css/templatemo-edu-meeting.css">
+<link rel="stylesheet" href="assets/css/owl.css">
+<link rel="stylesheet" href="assets/css/lightbox.css">
+
+
+<header class="header-area header-sticky">
+      <div class="container">
+          <div class="row">
+              <div class="col-12">
+                  <nav class="main-nav">
+                      <!-- ***** Logo Start ***** -->
+                      <a href="index.html" class="logo" style="background-color:#000000; color:#FFFFFF;">
+                    Imperial
+                        </a>
+                      <!-- ***** Logo End ***** -->
+                      <!-- ***** Menu Start ***** -->
+                      <ul class="nav" >
+                      <li class="scroll-to-section"><a href="inicio.php" class="logo" style="background-color:#000000; color:#FFFFFF;">INICIO</a></li>
+                          <li class="scroll-to-section"><a href="carreras.php" class="logo" style="background-color:#000000; color:#FFFFFF;">Carreras</a></li>
+                          <li class="scroll-to-section"><a href="cursos.php" class="logo" style="background-color:#000000; color:#FFFFFF;">Cursos</a></li>
+                          <li class="scroll-to-section"><a href="extraescolares.php" class="logo" style="background-color:#000000; color:#FFFFFF;">Extraescolares</a></li>
+                          <li class="scroll-to-section"><a href="maestros.php" class="logo" style="background-color:#000000; color:#FFFFFF;">Maestros</a></li>
+                          <li class="scroll-to-section"><a href="eventos.php" class="logo" style="background-color:#000000; color:#FFFFFF;">Eventos</a></li>
+                          <li class="scroll-to-section"><a href="servicios.php" class="logo" style="background-color:#000000; color:#FFFFFF;">Servicios Escolares</a></li>
+                          <li class="scroll-to-section"><a href="informacion.php" class="logo" style="background-color:#000000; color:#FFFFFF;">informacion</a></li>
+                      </ul>        
+                  </nav>
+              </div>
+          </div>
+      </div>
+  </header>
 
 TemplateMo 569 Edu Meeting
 
@@ -61,4 +91,3 @@ https://templatemo.com/tm-569-edu-meeting
   </section>
 </body>
 
-<?php include("template/pie.php");?>
--- servicios.php
+++ servicios.php
@@ -22,6 +22,43 @@
     <link rel="stylesheet" href="assets/css/animate.css">
     <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
 
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+<!-- Additional CSS Files -->
+<link rel="stylesheet" href="assets/css/fontawesome.css">
+<link rel="stylesheet" href="assets/css/templatemo-edu-meeting.css">
+<link rel="stylesheet" href="assets/css/owl.css">
+<link rel="stylesheet" href="assets/css/lightbox.css">
+
+
+<header class="header-area header-sticky">
+      <div class="container">
+          <div class="row">
+              <div class="col-12">
+                  <nav class="main-nav">
+                      <!-- ***** Logo Start ***** -->
+                      <a href="index.html" class="logo" style="background-color:#000000; color:#FFFFFF;">
+                    Imperial
+                        </a>
+                      <!-- ***** Logo End ***** -->
+                      <!-- ***** Menu Start ***** -->
+                      <ul class="nav" >
+                      <li class="scroll-to-section"><a href="inicio.php" class="logo" style="background-color:#000000; color:#FFFFFF;">INICIO</a></li>
+                          <li class="scroll-to-section"><a href="carreras.php" class="logo" style="background-color:#000000; color:#FFFFFF;">Carreras</a></li>
+                          <li class="scroll-to-section"><a href="cursos.php" class="logo" style="background-color:#000000; color:#FFFFFF;">Cursos</a></li>
+                          <li class="scroll-to-section"><a href="extraescolares.php" class="logo" style="background-color:#000000; color:#FFFFFF;">Extraescolares</a></li>
+                          <li class="scroll-to-section"><a href="maestros.php" class="logo" style="background-color:#000000; color:#FFFFFF;">Maestros</a></li>
+                          <li class="scroll-to-section"><a href="eventos.php" class="logo" style="background-color:#000000; color:#FFFFFF;">Eventos</a></li>
+                          <li class="scroll-to-section"><a href="servicios.php" class="logo" style="background-color:#000000; color:#FFFFFF;">Servicios Escolares</a></li>
+                          <li class="scroll-to-section"><a href="informacion.php" class="logo" style="background-color:#000000; color:#FFFFFF;">informacion</a></li>
+                      </ul>        
+                  </nav>
+              </div>
+          </div>
+      </div>
+  </header>
+
     <div class="contact-us section" id="contact">
     <div class="container">
       <div class="row">
