19252c481b49ddaeeb6d23778c6c3062cdddedfb
alexis guerra <65685428+alexis-guerra@users.noreply.github.com>
2023-12-05 21:04:35 -0800

    Revert "dawd"
    This reverts commit 69cd5710fe0a22e9dacdd9cf207154f8fd66a995.

--- constancias.php
+++ constancias.php
@@ -42,17 +42,12 @@
               <div class="col-lg-6 offset-lg-3">
                 <div class="section-heading">
 <<<<<<< HEAD
-<<<<<<< HEAD
                   <h2>Solicita Tu Constancia</h2>
                   <h6>Bienvenido al espacio de constancias de nuestra plataforma educativa. Llena Los Datos Para Comunicarte Con Nosotros.</h6>
 =======
                   <h6>Contact Us</h6>
                   <h2>Fill Out The Form Below To <span>Get</span> In <em>Touch</em> With Us</h2>
 >>>>>>> parent of 586b582 (92%)
-=======
-                  <h6>Contact Us</h6>
-                  <h2>Fill Out The Form Below To <span>Get</span> In <em>Touch</em> With Us</h2>
->>>>>>> parent of ecc71ff (Modificaciones)
                 </div>
               </div>
               <div class="col-lg-9">
@@ -70,53 +65,37 @@
                   <div class="col-lg-6">
                     <fieldset>
 <<<<<<< HEAD
-<<<<<<< HEAD
                       <input type="text" name="email" id="email" pattern="[^ @]*@[^ @]*" placeholder="Correo Electronico" required="">
 =======
                       <input type="text" name="email" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your Email" required="">
 >>>>>>> parent of 586b582 (92%)
-=======
-                      <input type="text" name="email" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your Email" required="">
->>>>>>> parent of ecc71ff (Modificaciones)
                     </fieldset>
                   </div>
                   <div class="col-lg-6">
                     <fieldset>
 <<<<<<< HEAD
-<<<<<<< HEAD
                       <input type="subject" name="subject" id="subject" placeholder="Asunto" autocomplete="on">
 =======
                       <input type="subject" name="subject" id="subject" placeholder="Subject" autocomplete="on">
 >>>>>>> parent of 586b582 (92%)
-=======
-                      <input type="subject" name="subject" id="subject" placeholder="Subject" autocomplete="on">
->>>>>>> parent of ecc71ff (Modificaciones)
                     </fieldset>
                   </div>
                   <div class="col-lg-12">
                     <fieldset>
-<<<<<<< HEAD
 <<<<<<< HEAD
                       <textarea name="message" type="text" class="form-control" id="message" placeholder="Descripcion" required=""></textarea>  
 =======
                       <textarea name="message" type="text" class="form-control" id="message" placeholder="Message" required=""></textarea>  
 >>>>>>> parent of 586b582 (92%)
-=======
-                      <textarea name="message" type="text" class="form-control" id="message" placeholder="Message" required=""></textarea>  
->>>>>>> parent of ecc71ff (Modificaciones)
                     </fieldset>
                   </div>
                   <div class="col-lg-12">
                     <fieldset>
 <<<<<<< HEAD
-<<<<<<< HEAD
                       <button type="submit" id="form-submit" class="main-button ">Enviar</button>
 =======
                       <button type="submit" id="form-submit" class="main-button ">Send Message Now</button>
 >>>>>>> parent of 586b582 (92%)
-=======
-                      <button type="submit" id="form-submit" class="main-button ">Send Message Now</button>
->>>>>>> parent of ecc71ff (Modificaciones)
                     </fieldset>
                   </div>
                 </div>
--- informacion.php
+++ informacion.php
@@ -47,7 +47,7 @@
               </h2>
               <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                  Dolor <strong>almesit amet</strong>, consectetur adipiscing elit, sed doesn't eiusmod tempor incididunt ut labore consectetur <code>adipiscing</code> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.
+                Descubre una educación de calidad, apoyo integral y oportunidades para destacar. Te ofrecemos más que clases; te brindamos un camino hacia el éxito profesional y personal. ¡Inscríbete ahora y comienza tu viaje hacia el logro de tus metas!
                 </div>
               </div>
             </div>
@@ -59,7 +59,8 @@
               </h2>
               <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                  Dolor <strong>almesit amet</strong>, consectetur adipiscing elit, sed doesn't eiusmod tempor incididunt ut labore consectetur <code>adipiscing</code> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.
+                Experimenta un entorno educativo único, donde el apoyo integral, enfoque práctico, desarrollo personalizado y una comunidad vibrante se entrelazan para brindarte una educación que va más allá de las expectativas. 
+                Únete a nosotros y descubre el poder transformador de de la institución. Tu futuro comienza aquí.
                 </div>
               </div>
             </div>
@@ -83,7 +84,11 @@
               </h2>
               <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                  You can also search on Google with specific keywords such as <code>templatemo business templates, templatemo gallery templates, admin dashboard templatemo, 3-column templatemo, etc.</code>
+                Al egresar de la institución, no solo obtendrás un título, sino un conjunto de habilidades distintivas que impulsarán tu éxito:
+                Desarrollarás competencias técnicas avanzadas para destacar en tu campo. Perfeccionarás el pensamiento crítico y analítico, esencial para abordar desafíos complejos. Tus habilidades de comunicación se afilarán, permitiéndote expresar ideas con claridad y persuasión.
+                Aprenderás a trabajar eficazmente en equipo, a adaptarte a entornos cambiantes con resiliencia y a liderar éticamente. Estimularás tu pensamiento creativo y te familiarizarás con las últimas herramientas digitales.
+                Fomentamos el desarrollo continuo y construimos conexiones profesionales valiosas. Tu tiempo aquí no solo te preparará para el éxito inmediato, sino que te equipará para prosperar en un mundo en constante evolución.
+                ¡Tu éxito comienza con habilidades que trascienden el aula!
                 </div>
               </div>
             </div>
